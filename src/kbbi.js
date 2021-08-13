177f80f93a585c27678d38040bbad4ca4e35fc65
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-08-13 07:07:42 +0700

    update the API

--- src/kbbi.js
+++ src/kbbi.js
@@ -1,10 +1,14 @@
 const JcAPI = require('jastinch-api-wrapper')
-
+const axios = require('axios')
+const userAgents = require('./tools/user-agents.js')
 const cari = async(keyword) => {
     if(!keyword) throw new Error('Provide the keyword/kata kunci!')
-    const data = await JcAPI.KBBI(keyword)
-    return data;
+    const get = await axios(`https://kbbi-api-zhirrr.vercel.app/api/kbbi?text=${keyword}`, {
+    headers: {
+        "User-Agent": userAgents[Math.floor(Math.random() * userAgents.length)]
+    }
+    })
+    const res = get.data
+    return res
 }
 module.exports = cari;
-// find some more useful functions at jastinch-api-wrapper! you can check at https://npmjs.com/package/jastinch-api-wrapper
-// lol 10 lines xD
